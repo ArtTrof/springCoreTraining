d0dedf897929022dc779187f95cda048aa3ad670
Artem Trofymenko <randallsng@gmail.com>
2023-01-20 14:23:16 +0100

    commit msg

--- src/main/java/org/example/MusicPlayer.java
+++ src/main/java/org/example/MusicPlayer.java
@@ -1,13 +1,59 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class MusicPlayer {
-    private Music music;
-    //IoC
-    public MusicPlayer(Music music) {
-        this.music = music;
+    private List<Music> musicList = new ArrayList<>();
+    private String name;
+    private int volume;
+
+    public MusicPlayer() {
     }
 
-    public void playMusic(){
-        System.out.println("Playing:"+music.getSong());
+    //IoC
+    public MusicPlayer(Music musicList) {
+        this.musicList = (List<Music>) musicList;
+    }
+
+
+
+    public void playMusic() {
+        for(Music music:musicList){
+            System.out.println(music.getSong());
+        }
+    }
+
+//    public Music getMusic() {
+//        return music;
+//    }
+//    public void setMusic(Music music) {
+//        this.music = music;
+//    }
+    public String getName() {
+        return name;
+    }
+
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    public List<Music> getMusicList() {
+        return musicList;
+    }
+
+    public  void setMusicList(List<Music> musicList) {
+        this.musicList = musicList;
+//        musicList.add((Music) this.musicList);
     }
 }
--- src/main/java/org/example/TetSpring.java
+++ src/main/java/org/example/TetSpring.java
@@ -6,9 +6,11 @@ public class TetSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
                 "applicationContext.xml");
+//this is lesson 7
+//        Music music= context.getBean("musicBean",Music.class);
+//        MusicPlayer musicPlayer = new MusicPlayer(music);
 
-        Music music= context.getBean("musicBean",Music.class);
-        MusicPlayer musicPlayer = new MusicPlayer(music);
+        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
         musicPlayer.playMusic();
 
         context.close();
